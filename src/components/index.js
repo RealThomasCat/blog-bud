bc0e960600a9eb653faca4b32be0616792447c5d
Yashveer Singh <yashveer.rtc@gmail.com>
2024-02-11 21:11:11 +0530

    Added RTE and Post form

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,18 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
+import Signup from "./Signup";
+import Login from "./Login";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Signup,
+  Login,
+  RTE,
+};
